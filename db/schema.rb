4503a7a06cba8f3b8ebb5d3467d1f41e90dfef2f
Amira Mostafa <amirafigla@gmail.com>
2024-11-25 22:05:06 +0000

    Remove Email Phone and twitter from friends

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_19_180139) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_25_212813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
-    t.string "phone"
-    t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
