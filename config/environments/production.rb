ffb0912d17d2a3fc3624c00935cc223927d5a448
Amira Mostafa <amirafigla@gmail.com>
2024-12-26 14:27:47 +0000

    Fix production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'www.rails-friends-4b50a9cc384d.herokuapp.com/', protocol: 'https' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV.fetch('SMTP_USER', nil),
+    password: ENV.fetch('SMTP_PASSWORD', nil),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
