85f31f8b9bc256206c0246f388ca624de828063c
Amira Mostafa <amirafigla@gmail.com>
2024-12-26 15:48:26 +0000

    Add domain to settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
+    domain: 'gmail.com',
     port: 587,
     user_name: ENV.fetch('SMTP_USER', nil),
     password: ENV.fetch('SMTP_PASSWORD', nil),
