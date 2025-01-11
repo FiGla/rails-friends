8ab013160ce218b2164e291b90772eb64291f0f9
Amira Mostafa <amirafigla@gmail.com>
2025-01-11 22:54:38 +0000

    Add logs in the mailer Modify task to perform later

--- app/mailers/birthday_notification_mailer.rb
+++ app/mailers/birthday_notification_mailer.rb
@@ -8,18 +8,21 @@ class BirthdayNotificationMailer < ApplicationMailer
   def birthday_notification_email(friend)
     @friend = friend
     @user = friend.user
+    Rails.logger.info "Sending birthday notification email to: #{friend.user.email}"
     mail(to: @user.email, subject: "Your friend's birthday is today!")
   end
 
   def reminder_birthday_notification_email(friend)
     @friend = friend
     @user = friend.user
+    Rails.logger.info "Sending birthday notification email to: #{friend.user.email}"
     mail(to: @user.email, subject: "Your friend's birthday is next Week!")
   end
 
   def child_birthday_notification_email(child)
     @child = child
     @user = child.friend.user
+    Rails.logger.info "Sending birthday notification email to: #{friend.user.email}"
     mail(to: @user.email, subject: "Your friend's child birthday is today!")
   end
 end
--- lib/tasks/email_notifications_job.rake
+++ lib/tasks/email_notifications_job.rake
@@ -3,6 +3,6 @@
 namespace :email_notifications do
   desc 'Run EmailNotificationJob'
   task birthday_reminder: :environment do
-    EmailNotificationJob.perform_now
+    EmailNotificationJob.perform_later
   end
 end
